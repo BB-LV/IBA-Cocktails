94b92ec44806502dcd85479a0e54a7558dad607f
brian.beardsley.89123@gmail.com <94084517+Bevelled@users.noreply.github.com>
2022-04-27 20:38:56 -0700

    Refactored code for more animations and transition

--- js/main.js
+++ js/main.js
@@ -24,76 +24,105 @@ const measures = document.querySelectorAll('.measure')
 
 //Page State 1 (Pick Main Group of Cocktails)
 
-////////////////////
-////////////////////
-////////////////////
+////////////////////////////////////////
+////////////////////NEED TO CLEANUP/////
+////////////////////////////////////////
 
-big3.forEach(i => {
-    i.addEventListener('click', e => {
-        let x = document.querySelector('.headingContainer');
+function hideShowingCards(){
+    drinkCards.forEach(card => {
+        if(card.classList.contains('drinkCardIntro')){
+            card.classList.toggle('drinkCardIntro')
+        }else if(card.classList.contains('drinkCardSlideIn')){
+                card.classList.toggle('drinkCardSlideIn')
+        }
+    })
+}
+
+function takeFloatOffArrays() {
+    dA.forEach(array => {
+        if(array.classList.contains('drinkArrayFloat')){
+            array.classList.toggle('drinkArrayFloat');
+        }
+        if(array.classList.contains('drinkArrayFloatD')){
+            array.classList.toggle('drinkArrayFloatD')
+        }
+    })
+}
+
+function removeCocktailLinkedClick() {
+    drinkNames.forEach(name => {
+        if(name.classList.contains('cocktailLinkClicked')){
+            name.classList.toggle('cocktailLinkClicked')
+        }
+    })
+}
+
+big3.forEach(indHeader => {
+    indHeader.addEventListener('click', listerName => {
         let drinkArrays = document.querySelectorAll('.drinkArray');
-        let r = document.querySelectorAll('.drinkCardCont')
-        if(i.classList.contains('clickedbig3')){    // <--- resets to state 1 if button clicked is already selected *****
-            x.classList.toggle('headingContainerMoved');
-            x.classList.toggle('headingContainerHover');
-            big3.forEach(y => {
-                y.classList.toggle('big3ContHover');
-                i.classList.toggle('big3ContMoved');
-            });
-            i.classList.toggle('clickedbig3');
-            drinkArrays.forEach(f => f.style.display = 'none');
-            drinkArrays.forEach(da => {      // <----- resets the .drinkArray's position to default
-                if(da.classList.contains('drinkArrayMoved')){
-                    da.classList.toggle('drinkArrayMoved')
+        let headingCont = document.querySelector('.headingContainer');
+            if(headingCont.classList.contains('headingContainerMovedF')){
+                if(indHeader.classList.contains('clickedBig3')){
+                    // same header clicked, reset (but the header is moved)
+                    headingCont.classList.toggle('headingContainerMovedF') /* removing */
+                    headingCont.classList.toggle('headingContainerMovedInitial') /* adding */
+                    indHeader.classList.toggle('clickedBig3')
+                    big3.forEach(indHeader4 => {
+                        indHeader4.classList.toggle('big3ContHover')
+                        indHeader4.children[1].classList.toggle('big3Hovered')
+                    })
+                    drinkArrays.forEach(drinkArr => {
+                        if(drinkArr.classList.contains('drinkArrayInitial')){ // <-- should be an outtro
+                            drinkArr.classList.toggle('drinkArrayInitial')
+                        }
+                    })
+                    hideShowingCards();
+                    takeFloatOffArrays();
+                    removeCocktailLinkedClick();
+                }else{
+                     // different header clicked (but the header is moved)
+                    big3.forEach(indHeader3 => {
+                        if(indHeader3 == indHeader){
+                            indHeader3.classList.toggle('clickedBig3')
+                        }else if(indHeader3.classList.contains('clickedBig3')){
+                            indHeader3.classList.toggle('clickedBig3')
+                        }
+                    })
+                    drinkArrays.forEach(drinkArr2 => {
+                        if(drinkArr2.classList.contains('drinkArrayInitial')){  // <-- should be an outtro
+                            drinkArr2.classList.toggle('drinkArrayInitial')
+                        }else if(drinkArr2.dataset.category == indHeader.dataset.category){
+                            drinkArr2.classList.toggle('drinkArrayInitial')
+                        }
+                    })
+                    hideShowingCards();
+                    takeFloatOffArrays();
+                    removeCocktailLinkedClick();
                 }
-            })
-            r.forEach(rr => {   // <---- toggles drink cards if they are showing
-                if(!rr.classList.contains('drinkCardContNone')){
-                    rr.classList.toggle('drinkCardContNone')
+            }else{ // <-- Heading Container not moved, initial move to stage 2.
+                if(headingCont.classList.contains('headingContainerMovedInitial')){
+                    headingCont.classList.toggle('headingContainerMovedInitial')
                 }
-            })
-        }else if(x.classList.contains('headingContainerMoved')){ // rearranges state 2  *****
-            big3.forEach(a => {
-                a.classList.remove('clickedbig3');
-            });
-            i.classList.toggle('clickedbig3')
-            drinkArrays.forEach(g => g.style.display = 'none');
-            if(i.classList.contains('unforgetHCont')){
-                drinkArrays[0].style.display = 'flex';
-            }else if(i.classList.contains('contempCHCont')){
-                drinkArrays[1].style.display = 'flex';
-            }else{
-                drinkArrays[2].style.display ='flex';
-            };
-            drinkArrays.forEach(da => {      // <----- resets the .drinkArray's position to default
-                if(da.classList.contains('drinkArrayMoved')){
-                    da.classList.toggle('drinkArrayMoved')
+                headingCont.classList.toggle('headingContainerMovedF')
+                big3.forEach(indHeader2 => {
+                    if(indHeader2 == indHeader){
+                        indHeader.classList.toggle('clickedBig3')
+                    }
+                    if(indHeader2.classList.contains('big3ContHover')){
+                        indHeader2.classList.toggle('big3ContHover')
+                        indHeader2.children[1].classList.toggle('big3Hovered')
+                    }
+                })
+                if(indHeader.classList.contains('unforgetHCont')){
+                    document.querySelector('.unforgetHDrinks').classList.toggle('drinkArrayInitial');
+                }else if(indHeader.classList.contains('contempCHCont')){
+                    document.querySelector('.contempCHDrinks').classList.toggle('drinkArrayInitial');
+                }else{
+                    document.querySelector('.newEDHDrinks').classList.toggle('drinkArrayInitial');
                 }
-            })
-            r.forEach(rr => {   // <---- toggles drink cards if they are showing
-                if(!rr.classList.contains('drinkCardContNone')){
-                    rr.classList.toggle('drinkCardContNone')
-                }
-            })
-        }else{      // state 1 is assumed here. transitions state 1 to state 2.*****
-            x.classList.toggle('headingContainerMoved');
-            x.classList.toggle('headingContainerHover');
-            big3.forEach(y => {
-                y.classList.toggle('big3ContHover');
-                i.classList.toggle('big3ContMoved');
-            });
-            i.classList.toggle('clickedbig3');
-            x.classList.toggle('headingContainerClickedPerspective');
-            if(i.classList.contains('unforgetHCont')){
-                drinkArrays[0].style.display = 'flex';
-            }else if(i.classList.contains('contempCHCont')){
-                drinkArrays[1].style.display = 'flex';
-            }else{
-                drinkArrays[2].style.display ='flex';
-            };
-        };
-    }
-)});
+            }
+    })
+})
 
 ////////////////////
 ////////////////////
@@ -105,36 +134,59 @@ big3.forEach(i => {
 ////////////////////
 ////////////////////
 
-drinkNames.forEach(indDrink => {
-    indDrink.addEventListener('click', clicked => {
-        drinkCards.forEach(indDCard => {
-            if(indDCard.dataset.dname == indDrink.dataset.dname){
-                if(dA[0].classList.contains('drinkArrayMoved')){
-                    if(!indDCard.classList.contains('drinkCardContNone')){  
-                        dA.forEach(drinkArr => {
-                            drinkArr.classList.toggle('drinkArrayMoved')
-                        });
-                        indDCard.classList.toggle('drinkCardContNone');
-                    }else{                                                
-                        indDCard.classList.toggle('drinkCardContNone');
-                    };
-                }else{
-                    dA.forEach(drinkArr2 => {
-                        if(!drinkArr2.classList.contains('drinkArrayMoved')){
-                            drinkArr2.classList.toggle('drinkArrayMoved')
-                        }
-                    })
-                    indDCard.classList.toggle('drinkCardContNone');
-                };
-            }else{
-                if(!indDCard.classList.contains('drinkCardContNone')){
-                    indDCard.classList.toggle('drinkCardContNone');
-                };
-            };
-        })
 
+drinkNames.forEach(dName => {
+    dName.addEventListener('click', nameListener => {
+        drinkCards.forEach(dCard => {
+            function cardExit(){   // <-- Removes Card's Intro Animation Classes and gives an exit class.
+                if(dCard.classList.contains('drinkCardSlideIn')){
+                    dCard.classList.toggle('drinkCardSlideIn')
+                    dCard.classList.toggle('drinkCardSlideOut')
+                }else if(dCard.classList.contains('drinkCardIntro')){
+                    dCard.classList.toggle('drinkCardIntro')
+                    dCard.classList.toggle('drinkCardSlideOut')
+                }
+            }
+            if(dCard.dataset.dname == dName.dataset.dname){
+                dA.forEach(drinkArray1 => {
+                    if(drinkArray1.classList.contains('drinkArrayInitial')){
+                        if(!drinkArray1.classList.contains('drinkArrayFloat')){
+                            // its assumed this is initial stage 2
+                            if(dCard.classList.contains('drinkCardSlideOut')){
+                                dCard.classList.toggle('drinkCardSlideOut');
+                            }
+                            if(drinkArray1.classList.contains('drinkArrayDown')){
+                                drinkArray1.classList.toggle('drinkArrayDown');
+                            }
+                            drinkArray1.classList.toggle('drinkArrayFloat');
+                            dName.classList.toggle('cocktailLinkClicked');
+                            dCard.classList.toggle('drinkCardIntro');
+                        }else{
+                            if(dName.classList.contains('cocktailLinkClicked')){
+                                cardExit();
+                                drinkArray1.classList.toggle('drinkArrayFloat');
+                                drinkArray1.classList.toggle('drinkArrayDown');
+                            }else{
+                                cardExit();
+                                dCard.classList.toggle('drinkCardSlideIn');
+                                drinkNames.forEach(name2 => {
+                                    if(name2.classList.contains('cocktailLinkClicked')){
+                                        name2.classList.toggle('cocktailLinkClicked');
+                                    }
+                                })
+                            }
+                            dName.classList.toggle('cocktailLinkClicked');
+                        }
+
+                    }
+                })
+            }else{
+                // Every card that is not tied to the drink name being clicked
+                cardExit();
+            }
+        })
     })
-});
+})
 
 ////////////////////
 ////////////////////
@@ -149,7 +201,7 @@ drinkNames.forEach(indDrink => {
 revealButtons.forEach(indButton => {
     indButton.addEventListener('click', buttonClicked => {
         drinkCards.forEach(indDCard2 => {
-            if(!indDCard2.classList.contains('drinkCardContNone')){
+            if(indDCard2.classList.contains('drinkCardIntro')){
                 for(let count = 0; count <indDCard2.children.length; count++){
                     if(indDCard2.children[count].classList.contains('methodCont')){
                         indDCard2.children[count].classList.toggle('methodContNone');
@@ -161,8 +213,10 @@ revealButtons.forEach(indButton => {
 });
 
 ////////////////////
-//Mutation Observer For Method Containers (Hides Method Containers Every Time A Card Changes)
+//Mutation Observers 
 ////////////////////
+
+// For Method Containers (Hides Method Containers Every Time A Card Changes)
 
 const observer = new MutationObserver(function() {
     let methodContainers = document.querySelectorAll('.methodCont')
@@ -178,8 +232,6 @@ drinkCards.forEach(drinkC => {
         attributes: true,
     })
 })
-
-
 
 ////////////////////
 ////////////////////
